ed123cfe2fa5b274eac74a81787cb9d95eaa906f
yongkiagustin <yongkiagustin92@gmail.com>
2020-06-09 22:19:16 +0700

    menambah handling inputan jika kosong
    menambah handling inputan jika kosong, menambah fungsi enkripsi dan decripsi passowrd dan handling jika password tidak sama

--- app/src/main/java/id/yongki/layananqta/RegisterActivity.java
+++ app/src/main/java/id/yongki/layananqta/RegisterActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -18,40 +20,84 @@ import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private Button regisBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final EditText namaLengkap, email, nohp, password, repassword;
+        Button regisBtn;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         regisBtn = findViewById(R.id.register_regisbtn);
+        namaLengkap = findViewById(R.id.register_namaLengkap);
+        email = findViewById(R.id.register_etemail);
+        nohp = findViewById(R.id.register_etnotlp);
+        password = findViewById(R.id.register_etpassword);
+        repassword = findViewById(R.id.register_etrepassword);
+
+
         regisBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-// Access a Cloud Firestore instance from your Activity
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                String gnama = namaLengkap.getText().toString();
+                String gemail = email.getText().toString();
+                String gnohp = nohp.getText().toString();
+                String gpassword = password.getText().toString();
+                String grepassword = repassword.getText().toString();
+                String epass = " ";
 
-                // Create a new user with a first and last name
-                Map<String, Object> user = new HashMap<>();
-                user.put("name", "Yongki Agustin");
-                user.put("NoHp", "0");
-                user.put("", 50000);
-                user.put("price_to", 500000);
+                if (gnama.isEmpty()) {
+                    namaLengkap.setError("Nama lengkap tidak boleh kosong");
+                } else if (gemail.isEmpty()) {
+                    email.setError("Email tidak boleh kosong");
+                }else if (gnama.isEmpty()) {
+                    nohp.setError("Nomor tidak boleh kosong");
+                }else if (gpassword.isEmpty()) {
+                    password.setError("Password tidak boleh kosong");
+                }
+                else if(!gpassword.equals(grepassword)){
+                    Toast toast = Toast.makeText(getApplicationContext(), "Password tidak cocok!", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                else {
+                    try {
+                        epass = AESCrypt.encrypt(gpassword);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    //koneksi ke firebase
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-// Add a new document with a generated ID
-                db.collection("type")
-                        .add(user)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d("tes", "DocumentSnapshot added with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w("tes", "Error adding document", e);
-                            }
-                        });
+                    // membuat data user baru ke table users
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("nama", gnama);
+                    user.put("email", gemail);
+                    user.put("Nohp", gnohp);
+                    user.put("password", epass);
+                    user.put("deskripsi", "");
+                    // Add a new document with a generated ID
+                    db.collection("users")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d("tes", "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("tes", "Error adding document", e);
+
+                                }
+                            });
+                    Toast toast = Toast.makeText(getApplicationContext(), "Selamat, anda sudah terdaftar! Silahkan melakukan login", Toast.LENGTH_LONG);
+                    toast.show();
+
+
+                }
             }
         });
     }
