e88b58a1f99e45d55136afd816c60167367a177c
yongkiagustin <yongkiagustin@gmail.com>
2020-07-09 06:05:36 +0700

    check

--- app/src/main/java/id/yongki/layananqta/AccountActivity.java
+++ app/src/main/java/id/yongki/layananqta/AccountActivity.java
@@ -13,4 +13,5 @@ public class AccountActivity extends AppCompatActivity {
 
         // TODO BUAT LAYOUTNYA
     }
+
 }
--- app/src/main/java/id/yongki/layananqta/FormBiodataActivity.java
+++ app/src/main/java/id/yongki/layananqta/FormBiodataActivity.java
@@ -11,12 +11,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Document;
 
@@ -48,6 +52,7 @@ public class FormBiodataActivity extends AppCompatActivity {
                 String nohp = etnohp.getText().toString();
                 String alamat = etalamat.getText().toString();
                 String email = mUser.getEmail();
+
 
                 //membuat data user ke firestore
                 Map<String, Object> user = new HashMap<>();
