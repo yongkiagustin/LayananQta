3063138a14f92a9a772fae7564d1b0907437ea9c
yongkiagustin <yongkiagustin92@gmail.com>
2020-07-09 16:46:21 +0700

    update biodataActivity

--- app/src/main/java/id/yongki/layananqta/RegisterActivity.java
+++ app/src/main/java/id/yongki/layananqta/RegisterActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,13 +23,19 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText email, password, repassword;
+    public EditText etnama, etnohp, etalamat, etemail, etpassword, etrepassword;
     private ProgressBar progressBar;
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
     private FirebaseAuth mAuth;
     boolean isPlay = false;
 
@@ -41,12 +48,13 @@ public class RegisterActivity extends AppCompatActivity {
         TextView login = findViewById(R.id.register_tvlogin);
         progressBar = findViewById(R.id.register_progressbar);
         Button regisBtn = findViewById(R.id.register_regisbtn);
-        email = findViewById(R.id.register_etemail);
-        password = findViewById(R.id.register_etpassword);
-        repassword = findViewById(R.id.register_etrepassword);
+        etnama = findViewById(R.id.register_etnama);
+        etnohp = findViewById(R.id.register_etnohp);
+        etalamat = findViewById(R.id.register_etalamat);
+        etemail = findViewById(R.id.register_etemail);
+        etpassword = findViewById(R.id.register_etpassword);
+        etrepassword = findViewById(R.id.register_etrepassword);
         ImageView icEye = findViewById(R.id.register_ic_eye);
-
-
 
 
         icEye.setBackgroundResource(R.drawable.ic_eye_hide);
@@ -56,10 +64,10 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (isPlay) {
                     v.setBackgroundResource(R.drawable.ic_eye_hide);
-                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    etpassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
                 } else {
                     v.setBackgroundResource(R.drawable.ic_outline_eye_24);
-                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    etpassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
                 }
                 isPlay = !isPlay;
             }
@@ -69,9 +77,13 @@ public class RegisterActivity extends AppCompatActivity {
         regisBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String gemail = email.getText().toString();
-                final String gpassword = password.getText().toString();
-                final String grepassword = repassword.getText().toString();
+                final String gnama = etnama.getText().toString();
+                final String gnohp = etnohp.getText().toString();
+                final String galamat = etalamat.getText().toString();
+                final String gemail = etemail.getText().toString();
+                String gpassword = etpassword.getText().toString();
+                String grepassword = etrepassword.getText().toString();
+
                 final String semail = getString(R.string.email_kosong);
                 final String spassword = getString(R.string.pass_kosong);
                 final String spassTidakCocok = getString(R.string.pass_tidak_cocok);
@@ -79,21 +91,40 @@ public class RegisterActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.VISIBLE);
 
                 if (gemail.isEmpty()) {
-                    email.setError(semail);
+                    etemail.setError(semail);
                     progressBar.setVisibility(View.GONE);
 
 
                 } else if (gpassword.isEmpty()) {
-                    password.setError(spassword);
+                    etpassword.setError(spassword);
                     progressBar.setVisibility(View.GONE);
                 } else if (!gpassword.equals(grepassword)) {
                     progressBar.setVisibility(View.GONE);
-                    repassword.setError(spassTidakCocok);
+                    etrepassword.setError(spassTidakCocok);
                 } else {
+
                     mAuth.createUserWithEmailAndPassword(gemail, gpassword)
                             .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                                 @Override
                                 public void onSuccess(AuthResult authResult) {
+//                                    //membuat data user ke firestore
+//                                    Map<String, Object> user = new HashMap<>();
+//                                    user.put("nama", gnama);
+//                                    user.put("email", gemail);
+//                                    user.put("nohp", gnohp);
+//                                    user.put("alamat", galamat);
+//                                    db.collection("users").document(mUser.getUid()).set(user)
+//                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                                                @Override
+//                                                public void onSuccess(Void aVoid) {
+//                                                }
+//                                            })
+//                                            .addOnFailureListener(new OnFailureListener() {
+//                                                @Override
+//                                                public void onFailure(@NonNull Exception e) {
+//                                                    Log.w("", "Error adding document", e);
+//                                                }
+//                                            });
                                     showDialog();
                                 }
                             })
@@ -135,6 +166,7 @@ public class RegisterActivity extends AppCompatActivity {
                 .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
+
                         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                         finish();
                     }
